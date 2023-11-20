875f91557833a317af84cf0d287855ccc02085ed
Tashiro Hiroaki <147316021+TashiroHiroaki@users.noreply.github.com>
2023-11-20 21:24:59 +0900

    CSS以外簡易実装。

--- script.js
+++ script.js
@@ -1,7 +1,168 @@
 window.addEventListener("DOMContentLoaded", () => {
-    　　const audio = document.getElementById("audio-1");
+    　　const audio = document.getElementById("button-1");
     　audio.addEventListener('click', function(){
-    　　document.getElementById("sounds-1").currentTime = 0;
-    　　document.getElementById("sounds-1").play();
+    　　document.getElementById("audio-1").currentTime = 0;
+    　　document.getElementById("audio-1").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-2");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-2").currentTime = 0;
+    　　document.getElementById("audio-2").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-3");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-3").currentTime = 0;
+    　　document.getElementById("audio-3").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-4");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-4").currentTime = 0;
+    　　document.getElementById("audio-4").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-5");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-5").currentTime = 0;
+    　　document.getElementById("audio-5").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-6");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-6").currentTime = 0;
+    　　document.getElementById("audio-6").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-7");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-7").currentTime = 0;
+    　　document.getElementById("audio-7").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-8");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-8").currentTime = 0;
+    　　document.getElementById("audio-8").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-9");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-9").currentTime = 0;
+    　　document.getElementById("audio-9").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-10");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-10").currentTime = 0;
+    　　document.getElementById("audio-10").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-11");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-11").currentTime = 0;
+    　　document.getElementById("audio-11").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-12");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-12").currentTime = 0;
+    　　document.getElementById("audio-12").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-13");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-13").currentTime = 0;
+    　　document.getElementById("audio-13").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-14");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-14").currentTime = 0;
+    　　document.getElementById("audio-14").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-15");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-15").currentTime = 0;
+    　　document.getElementById("audio-15").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-16");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-16").currentTime = 0;
+    　　document.getElementById("audio-16").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-17");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-17").currentTime = 0;
+    　　document.getElementById("audio-17").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-18");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-18").currentTime = 0;
+    　　document.getElementById("audio-18").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-19");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-19").currentTime = 0;
+    　　document.getElementById("audio-19").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-20");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-20").currentTime = 0;
+    　　document.getElementById("audio-20").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-21");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-21").currentTime = 0;
+    　　document.getElementById("audio-21").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-22");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-22").currentTime = 0;
+    　　document.getElementById("audio-22").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-23");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-23").currentTime = 0;
+    　　document.getElementById("audio-23").play();
+    　});
+    });
+window.addEventListener("DOMContentLoaded", () => {
+    　　const audio = document.getElementById("button-24");
+    　audio.addEventListener('click', function(){
+    　　document.getElementById("audio-24").currentTime = 0;
+    　　document.getElementById("audio-24").play();
     　});
     });
